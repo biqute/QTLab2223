a941b518fa4820fd6cb70115d0aeb41f10832e3e
Rocco Suanno <xroccosuannox@gmail.com>
2022-12-11 15:36:04 +0000

    Added a decription and a demo of the ManageInstrument class

--- TWPAandQubit/README.md
+++ TWPAandQubit/README.md
@@ -2,6 +2,75 @@
 Laboratory of Solid State and Quantum Technologies  
 Laboratory classe for the Master's degree in Physic at the University of Milano-Bicocca.
 
+##### Table of Contents  
+- [How we comunicate with the VNA device](#how-we-comunicate-with-the-VNA-device)  
+    - [A brief _operative_ guide on how the VNA works](#a-brief-operative-guide-on-how-the-VNA-works)
+    - [The _ManageInstrument_ class](#the-ManageInstrument-class)
+        - [Example: extablish a connection with the device and perform a measure](#example-extablish-a-connection-with-the-device-and-perform-a-measure)
+
+## How we comunicate with the VNA device
+Here we provide a class **ManageInstrument** that allows sending queries to a VNA device in LAN. By sending queries to the device, we can send instructions to the device and read the displayed data into arrays. 
+### A brief _operative_ guide on how the VNA works
+The device can work in two modes: "**Continuos** Scan" and "**Single** Scan".
+* In **Continuos scan** mode, the instrument makes consecutive scans as time passes. At each scan, it measures an entry of the S matrix (e.g. S21) for a certain number of equally spaced frequencies in the interval displayed on the device screen. Those measurements are stored in the memory of the instrument, where the device mantains _only the last_ "AVE" measurements (where "AVE" is a parameter _of the device_ that we can set). Once it performs a new scan, the oldest measurement in the memory is replaced by the new scan measurement.
+* Instead in **Single scan** mode we have to query the instrument each time we want to make a scan; this mode provides a better control of the measurement procedure, so we'll _always_ set the device on this mode.
+
+The value displayed on the screen for each frequency its an **average** of all the measurements stored in the device memory; as a consequence, before performing a measurement, you have to _clear_ the memory of the instrument.
+set the value of the _device parameter_ "AVE" and then perform "AVE" scans in order to fill the memory of the instrument. At last we read the values displayed on the screen, which are an average of the measurements performed.
+    
+**Schematically** when we use the device, we have to follow this procedure:
+* Clear the device memory and set the number of measurements the device stores (those are the values whose average will be displayed on the screen);
+* Set the displayed frequency interval (any scan is performed only along the interval [fmin,fmax] displayed on the screen);
+* Make as many scans as the number of measurements the device stores (we set this parametere before) and **then** read the data displayed on the screen.
+
+The "single_scan(...)" method in the "ManageInstrument" class makes the device follow this procedure.
+ 
+## The _ManageInstrument_ class
+With this class you can create an object that allows you to communicate with the instrument.
+```bash
+ManageInstrument
+│  
+├── instr              # pyvisa.Resource object that contains a reference to the
+|                      # device and allows sending queries with the method instr.query()
+├── single_scan()      # A method that sets the desired device parameters, performs
+                       # the desired number of scans and returns the values displayed on
+                       # the device screen into arrays
+```
+### Example: extablish a connection with the device and perform a measure 
+```python
+"""ESTABLISH A CONNECTION WITH THE DEVICE"""
+import vna
+import matplotlib.pyplot as plt
+IP = "<device_address>"
+instrument = vna.ManageInstrument(IP)
+
+"""SET THE DESIRED PARAMETER BEFORE PERFORMING THE MEASUREMENTS"""
+# Set [fmin,fmax] that is the frequency interval along which the scans are performed
+fmin = 1e9
+fmax = 3e9
+# Set the power of the monotone waves sent into the port 1 to measure the selected matrix element (e.g. S21)
+powerdBm = -15 #dBm
+# Set the number of equally spaced frequencies in the interval [fmin,fmax] sampled at each scan
+npoints = 10
+# Set the number of scans performed (that is the number of measurements whose average is returned)
+navgs = 2
+
+"""
+PERFORM THE MEASURE
+We call the single_scan() method with the above parameters.
+The method returns:
+-freq: an array containing the sampled equally spaced frequencies.
+-I and Q: respectively the real and the imaginary part of the matrix element values corresponding to the frequencies in "freq".
+"""
+freq, I, Q = instrument.single_scan(fmin,fmax,powerdBm,npoints,navgs)
+# Here we calculate the matrix element in dB, form its real and imaginary parts
+S21 = vna.IQ_to_S21(I,Q)
+
+plt.plot(freq,S21)
+plt.show()
+```
+
+
 ## Contributors
 
 - MatteoOrlandoni	(mail: [m.orlandoni@campus.unimib.it](m.orlandoni@campus.unimib.it))
--- TWPAandQubit/src/vna.py
+++ TWPAandQubit/src/vna.py
@@ -7,72 +7,70 @@ class ManageInstrument:
     This class allows creating an Object with:
     - Just one field "instr" that allows sending queries to the instrument: instr.query("<query in SCPI language>")
     - A bunch of methods that make simple sending queries to the instrument, without explicitly writing the SCPI commands 
-    - A method "single_scan(...)" that makes the instrument measure the S21 spectrum (see more in the method definition)
+    - A method "single_scan(...)" that makes the instrument measure the S21 spectrum (more informations in the method definition)
     """
-    # HOW THE INSTRUMENT WORKS (operatively):
-    # In [Continuos scan] mode, the instrument makes consecutive scans of the displayed frequency interval.
-    # At each scan, it measures S21 for each frequency in the interval. The results go in the memory of
-    # the instrument, where it stores the LAST "navgs" measures. Once it performs a new scan, the oldest
-    # measurement in the memory is replaced by the new scan measurement.
-    # The value displayed on the screen its an average of all the "navgs" measurements stored in the memory.  
-    #
-    # HOW WE WANT TO USE IT:
-    # We want to use it in [Single scan] mode. First we clear the memory of the instrument, then we perform
-    # "navgs" scans that fill the memory of the instrument and at last we read the values displayed on the screen,
-    # which are an average of the "navgs" measurements performed.
-    # 
-    # Before performing the task above, we query the instrument setting the displayed frequency interval, the number
-    # of equidistant frequency to sample and "navgs" (the number of scans performed).
-    # 
-    # The "singleScan(...)" method makes the instrument do it.
 
     def __init__(self, ip):
-        # Open connection to instrument
+        """Open connection to instrument"""
         rm = visa.ResourceManager()
         self.instr = rm.open_resource("TCPIP0::"+str(ip)+"::inst0::INSTR")
+        
+        pass
 
     def close(self, ip):
-        # Close connection to instrument
+        """Close connection to instrument"""
         self.instr.close()
     
     def get_id(self):
-        #get the name of the istrument  
+        """Get the name of the istrument"""
         self.instr.query("*IDN?")
 
     def reset(self):
-        #Reset any query
+        """Reset any query sent to the instrument"""
         self.instr.query('*RST')
     
     def single_scan(self,fmin,fmax,powerdBm,npoints,navgs): 
         """
-        Usage: <da scrivere>
+        Tells the device to perform "navgs" times a scan of the S21 spectrum in the frequency span [fmin,fmax]
+        by sampling "npoints" equal spaced frequencies.
+        This function returns the values displayed on the device screen (which are an average of the measurements) in 3 arrays:
+        "freq": List of the sampled frequencies;
+        "Q": Average of the Real part of the "navgs" measures taken at each frequency in "freq";
+        "I": Average of the Imaginary part of the "navgs" measures taken at each frequency in "freq";
+        
+        Use method IQ_to_S21(Q,I) to compute S21 in dB.
+        
+        "powerdBm" is the output power of the device in dBm (typical value: -15dBm) 
         """
 
-        #Set the the desired frequency range to display on the screen (measurements are performed on the displayed range)
+        # Set the the desired frequency range to display on the screen (measurements are performed on the displayed range)
         self.set_range(fmin,fmax)
         
-        #Set desired output power
+        # Set desired output power in dBm (typical value = -15dBm)
         self.set_power(powerdBm)
         
-        #Set number of sampled frequencies in the displayed frequency range (number of sweep points)
+        # Set number of sampled frequencies in the displayed frequency range (number of sampled frequencies at each sweep)
         self.set_sweep_points(npoints)
         
-        #Set number of averages (number of sweeps done in a single scan, it is the number of times
-        #the value at a frequency is measured and then those navgs values are averaged and then displayed)
+        # Set number of averages, that is the number of times the value at a same frequency is measured.
+        # For each frequency, those measurements are averaged and then displayed on the screen.
         self.set_average(navgs)
         
-        #Autoscale y axis displayed range
+        # Autoscale y axis displayed range
         self.autoscale()
 
-        #With the instructions above we prepared the display. Now we call a function that makes
-        #the "navgs" sweeps, so at the end of the function we'll see the averaged measurements on the screen.
+        # With the instructions above we prepared the display. Now we call a function that makes
+        # the "navgs" sweeps, so at the end of the function we'll see the averaged measurements on the screen.
         self.make_sweeps()
         
-        #Puts the displayed data into arrays
+        # Puts the displayed data into arrays.
+        # - "freq" is the array of the sampled frequencies
+        # - "I" and "Q" are respecively the (averaged) measurements of real(I) and imaginary(Q) parts
+        # of the selected matrix element (e.g. S21), corresponding to the frequencies in the "freq" array
         freq, I, Q = self.readIQ()
 
-        #Now we got the desired values, we set the display of the instrument in a way that is usefull
-        #to see real time changes on the trasmission line
+        # Now we got the desired values, we set the display of the instrument in a way that is usefull
+        # to see real time changes on the trasmission line
         self.instr.query('INIT:CONT 1;*OPC?') #Set continuos sweep
         self.set_average(1)
 
@@ -80,36 +78,36 @@ class ManageInstrument:
         return freq,I,Q
 
     def make_sweeps(self):
-        #Clear the memory of the instrument (past sweeps measurements)
+        # Clear the memory of the instrument (past sweeps measurements)
         self.instr.query('AVER:CLE;*OPC?')
         
-        #Set to single sweep (we want to do exactly "navgs" sweeps)
+        # Set to single sweep (we want to do exactly "navgs" sweeps)
         self.instr.query('INIT:CONT 0;*OPC?')
         
-        #Get "navgs", that is the number of sweep we want to make ("navgs" is set by the user in "singleScan(...)")
-        #NOTA: We want "navgs" be a variable in the memory of the instrument (that in the instrument its called "AVER"),
-        #because the instrument stores in its memory the measurements of the LAST "AVER" sweeps. So "navgs" cannot be
-        #a field of this class, but shall be a variable stored in the instrument memory. So to set or read that,
-        #we have to query the instrument.
+        # Get "navgs", that is the number of sweep we want to make.
+        # We want "navgs" be a variable in the memory of the instrument (that in the instrument its called "AVER"),
+        # because the instrument stores in its memory the measurements of the LAST "AVER" sweeps. So "navgs" cannot be
+        # a field of this class, but shall be a variable stored in the instrument memory. So to set or read that,
+        # we have to query the instrument.
         navgs = int(self.instr.query('AVER:COUN?'))
 
-        #Makes "navgs" sweeps, by querying "navgs" times the instrument to perform a single scan
+        # Makes "navgs" sweeps, by querying "navgs" times the instrument to perform a single sweep
         for i in np.arange(0,navgs):
-            #Perform a single sweep then wait (*OPC?)
+            # Perform a single sweep then wait (*OPC?)
             self.instr.query('INIT:IMM;*OPC?')
         
     def readIQ(self):
-        #Read I (real part of S21)
+        # Read I (real part of S21)
         self.instr.query('CALC:FORMat REAL;*OPC?')  #Display real part, in Linear scale
         self.autoscale()
         freq,I=self.read_data()  #Effectively reads what is displayed on the screen
 
-        #Read Q (imaginary part of S21)
+        # Read Q (imaginary part of S21)
         self.instr.query('CALC:FORMat IMAG;*OPC?')  #Display imaginary part, in Linear scale    
         self.autoscale()
         freq,Q=self.read_data() #Effectively reads what is displayed on the screen
 
-        #Displays S21dB
+        # Displays S21dB
         self.instr.query('CALC:FORMat MLOG;*OPC?')  #Display modulus (S21), in dB   
         self.autoscale()
 
@@ -121,17 +119,17 @@ class ManageInstrument:
         the X and Y coordinates of the points of the graph displayed on the screen.
         """
 
-        #Read Y values
+        # Read Y values
         data = self.instr.query('CALC:DATA:FDAT?')
-        #The result of the query above is a STRING containing values separated by "," (comma)
-        #So we have to SPLIT the string into an array of values
+        # The result of the query above is a STRING containing values separated by "," (comma),
+        # so we have to SPLIT the string into an array of values
         data.Split(",")
         
-        #Read X values
+        # Read X values
         freq = self.instr.query('FREQuency:DATA?')
         freq.Split(",")      
 
-        #Wait
+        # Wait
         self.instr.query('*OPC?')
 
         return freq, data
@@ -146,21 +144,21 @@ class ManageInstrument:
             print("MODALITA NON CONSENTITA")
 
     def autoscale(self):
-        #Make the trace 1 active
+        # Make the trace 1 active
         trace=1
         self.instr.query('CALC:PAR'+ str(trace)+':SEL;*OPC?')
-        #Autoscale the y axes
+        # Autoscale the y axes
         self.instr.query('DISP:WIND:TRAC'+str(trace)+':Y:AUTO;*OPC?')
 
     def set_sweep_points(self,npoints):
-        #Set the number of sample points in the displayed(!!) frequency range 
+        # Set the number of sample points in the displayed(!!) frequency range 
         npoints=round(abs(npoints))
         if (npoints > 10001):
             npoints = 10001
         self.instr.query('SWE:POIN '+str(npoints)+';*OPC?')    
 
     def set_power(self,powerdBm):
-        #Set the output power of the segnal 
+        # Set the output power of the segnal 
         if(powerdBm>3):
             powerdBm=3
         if(powerdBm<-45):
@@ -168,26 +166,25 @@ class ManageInstrument:
         self.instr.query('SOUR:POW '+str(round(powerdBm,1))+';*OPC?') 
 
     def set_range(self,min,max):
-        #Set the min of the freq range
+        # Set the min of the freq range
         self.instr.query('FREQ:START '+str(min)+';*OPC?');
-        #Set the max of the freq range
+        # Set the max of the freq range
         self.instr.query('FREQ:STOP '+str(max)+';*OPC?');
 
-    #if you set a high number you have to wait till the instrument execute at least navgs measure
+    # WARNING: if you set a high number for "navgs" you have to wait till the instrument execute at least navgs measure
     def set_average(self,navgs):
-        #Set the number of average for every frequency
+        # Set the number of average for every frequency
         navgs=round(abs(navgs))
         if(navgs>100):
             navgs=100
         self.instr.query('AVER:COUN '+str(navgs)+';*OPC?')
 
     def get_average(self):
-        #Get the number of average 
+        # Get the number of average 
         navgs=self.instr.query('AVER:COUN?')
         return navgs     
 
     def set_log_scale(self):
-
         self.instr.query('CALC:FORMat MLOG;*OPC?')
 
     def set_lin_scale(self):
